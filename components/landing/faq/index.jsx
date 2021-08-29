ffa28595f8a2395b765e4213bb04024099e8fce2
mojtaba am <mojyseo@gmail.com>
2021-08-30 01:08:23 +0430

    updated

--- components/landing/faq/index.jsx
+++ components/landing/faq/index.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+
 import styles from "styles/landing.module.css";
 import Collapse from "./collapse";
+import Fetch from "components/fetch";
 
-export default function Index({ data }) {
+export default function Index() {
+  const [data, setdata] = React.useState([]);
+
+  React.useEffect(() => {
+    Fetch.get("slider", (res) => {
+      setdata(res);
+    });
+  });
   return (
     <section className={styles.faq}>
       <h3>سؤالات متداول</h3>
--- components/landing/sponsors.jsx
+++ components/landing/sponsors.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import styles from "styles/landing.module.css";
 import VisibilitySensor from "react-visibility-sensor";
+import Fetch from "components/fetch";
 import { apiAddressBase } from "common";
 export default function Index() {
   const [data, setdata] = React.useState([]);
